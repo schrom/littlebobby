7a9d62e09470c2375c28803abf4c2f47c6af76e5
Jérôme Waibel <jerome.waibel@diva-e.com>
2021-09-20 17:03:40 +0200

    domain and vm stuff

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ How to use:
   `docker run --rm -it -p 5432:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=db  postgres:latest`
 
 * Run spring application
+
+  `./mvnw spring-boot:run`
+
 * Navigate to `http://localhost/` and have fun!
--- src/main/resources/initdb.sql
+++ src/main/resources/initdb.sql
@@ -13,6 +13,6 @@ insert into artikel values (73144402, 'Gummihammer', 6)
 insert into artikel values (73144403, 'Kleiner Hammer', 5)
 insert into artikel values (73144500, 'Bauarbeiter-Helm', 9)
 create table logindata (username varchar, email varchar, password varchar)
-insert into logindata values ('admin', 'admin@littlebobbyshop.com', '$2a$04$M.hXoMvV3YoT42wCrTQtsuvKcy3cbS5ElaLZM48KTUvK.Gh2F29ga') -- nimda
+insert into logindata values ('admin', 'admin@littlebobby.shop', '$2a$04$M.hXoMvV3YoT42wCrTQtsuvKcy3cbS5ElaLZM48KTUvK.Gh2F29ga') -- nimda
 insert into logindata values ('schrom', 'schrom@netpioneer.de', '$2a$04$d2ImSjqmEI0Kp0w3N38xkuuanmbvwRpEafqeHkRpYZnj03aByOLVe') -- banane
 insert into logindata values ('gerda',  'gerda@t-online.de', '$2a$04$CB4IFzdzzOTiUqOVn1hp4OF7VqruvOF75phGgYyztcvIXwiNO0ECS') -- geheim
